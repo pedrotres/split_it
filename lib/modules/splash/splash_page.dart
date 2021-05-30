e6c3a9a9606ac53dee19bc11b0115369c8d2a748
pedrotres <52545592+pedrotres@users.noreply.github.com>
2021-05-30 18:09:04 -0300

    Tela de Splash

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:split_it/theme/app_theme.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("SplashPage"),
+      backgroundColor: AppTheme.colors.background,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppTheme.gradients.background,
+        ),
+        child: SafeArea(
+          top: true,
+          bottom: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Opacity(
+                        opacity: 0.2,
+                        child: Image.asset(
+                          "assets/images/retangulo_direito.png",
+                          width: 198,
+                          height: 98,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    children: [
+                      Opacity(
+                        opacity: 0.2,
+                        child: Image.asset(
+                          "assets/images/retangulo_direito.png",
+                          width: 114,
+                          height: 54,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              Image.asset(
+                "assets/images/logo.png",
+                width: 128,
+                height: 112,
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Opacity(
+                        opacity: 0.2,
+                        child: Image.asset(
+                          "assets/images/retangulo_esquerdo.png",
+                          width: 198,
+                          height: 98,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Opacity(
+                        opacity: 0.2,
+                        child: Image.asset(
+                          "assets/images/retangulo_esquerdo.png",
+                          width: 114,
+                          height: 54,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/theme/app_gradients.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+abstract class AppGradients {
+  Gradient get background;
+}
+
+class AppGradientsDefault implements AppGradients {
+  @override
+  Gradient get background => LinearGradient(
+        colors: [
+          Color(0XFF40B38C),
+          Color(0XFF45CC93),
+        ],
+        transform: GradientRotation(2.35619 * pi),
+      );
+}
