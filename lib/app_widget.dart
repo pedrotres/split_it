a73bd9b5a1ccf1e82d3a37f339ee9f3a3f8e8242
pedrotres <52545592+pedrotres@users.noreply.github.com>
2021-05-30 16:19:14 -0300

    Initial commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:split_it/modules/splash/splash_page.dart';
+
+class AppWidge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Split.IT",
+      home: SplashPage(),
+    );
+  }
+}
