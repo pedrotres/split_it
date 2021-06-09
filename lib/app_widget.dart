3862da88bd293df137f02254450db0f792b6f9e2
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2021-06-09 14:53:24 -0300

    implementação inicial da tela de login

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:split_it/modules/splash/splash_page.dart';
+import 'package:split_it/modules/login/login_page.dart';
 
 class AppWidge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Split.IT",
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
